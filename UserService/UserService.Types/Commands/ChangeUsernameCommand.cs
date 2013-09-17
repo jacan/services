6c37b7fa8e554d4592d25af853ec3a87de709373
jacan <jckandersen@gmail.com>
2013-09-17 17:40:00 +0200

    Added new command, change username request

---
+++ UserService/UserService.Types/Commands/ChangeUsernameCommand.cs
@@ -0,0 +1,17 @@
+﻿using JacksFramework.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserService.Types.Commands
+{
+    public class ChangeUsernameCommand : CommandBase
+    {
+        public Guid UserId { get; set; }
+        
+        public string UserName { get; set; }
+        public string NewUserNameRequested { get; set; }
+    }
+}
