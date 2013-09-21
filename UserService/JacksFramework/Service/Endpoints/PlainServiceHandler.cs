908b35702d823bbe347837f6612d4d844f4346c3
jacan <jckandersen@gmail.com>
2013-09-21 17:53:48 +0200

    Added a plain (parallel) servicehandler that executes multiple sinks

---
+++ UserService/JacksFramework/Service/Endpoints/PlainServiceHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus;
+
+namespace JacksFramework.Service.Endpoints
+{
+    public abstract class PlainServiceHandler : IHandleMessages<EventBase>
+    {
+        private readonly IEnumerable<IMessageHandleSink> _messageSinks;
+        
+        public PlainServiceHandler(IEnumerable<IMessageHandleSink> messageSinks)
+        {
+            _messageSinks = messageSinks;
+        }
+
+        public void Handle(EventBase message)
+        {
+            _messageSinks.AsParallel()
+                .ForAll(x => x.Handle(message));
+        }
+    }
+}
