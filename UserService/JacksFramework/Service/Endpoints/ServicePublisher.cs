530c54e0c06e1168f0810b4631fdca7e59cfec02
jacan <jckandersen@gmail.com>
2013-09-21 17:52:40 +0200

    Cleanup and tabs in ServicePublisher

--- UserService/JacksFramework/Service/Endpoints/ServicePublisher.cs
+++ UserService/JacksFramework/Service/Endpoints/ServicePublisher.cs
@@ -1,24 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NServiceBus;
+﻿using NServiceBus;
 
 namespace JacksFramework.Service.Publisher
 {
-    public class ServicePublisher : IConfigureThisEndpoint, AsA_Publisher
-    {
-        protected IBus _bus;
+	public class ServicePublisher : IConfigureThisEndpoint, AsA_Publisher
+	{
+		protected IBus _bus;
 
-        public ServicePublisher(IBus bus)
-        {
-            _bus = bus;
-        }
+		public ServicePublisher(IBus bus)
+		{
+			_bus = bus;
+		}
 
-        public void Publish(IMessage message)
-        {
-            _bus.Publish(message);
-        }
-    }
+		public void Publish(IEvent eventMessage)
+		{
+			_bus.Publish(eventMessage);
+		}
+	}
 }
