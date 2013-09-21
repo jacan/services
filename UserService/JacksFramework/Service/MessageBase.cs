e0b0efa78d068e4b0425a88ef5aad7d0d0c199a9
jacan <jckandersen@gmail.com>
2013-09-21 17:46:48 +0200

    Added messagebase

---
+++ UserService/JacksFramework/Service/MessageBase.cs
@@ -0,0 +1,26 @@
+﻿using NServiceBus;
+using System;
+
+namespace JacksFramework.Service
+{
+    public interface IMessageBase
+    {
+        Guid MessageId { get; set; }
+        DateTime Timestamp { get; set; }
+    }
+
+    public class MessageBase : IMessageBase
+    {
+        public Guid MessageId
+        {
+            get;
+            set;
+        }
+
+        public DateTime Timestamp
+        {
+            get;
+            set;
+        }
+    }
+}
