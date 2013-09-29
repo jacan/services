fd67e3d2ffabdd73549a56b4dc194fc59ec68073
jacan <jckandersen@gmail.com>
2013-09-29 16:25:50 +0200

    Added command distributor

---
+++ UserService/JacksFramework/Service/CommandDistributor.cs
@@ -0,0 +1,37 @@
+﻿using NServiceBus;
+using System.Collections.Concurrent;
+
+namespace JacksFramework.Service
+{
+	public interface IDistrubute
+	{
+		void Add(IMessageBase command);
+		void Distribute();
+	}
+
+	public class CommandDistributor : IDistrubute
+	{
+		protected ConcurrentBag<IMessageBase> _commands;
+		protected IBus _bus;
+
+		public CommandDistributor(IBus bus)
+		{
+			_bus = bus;
+		}
+
+		public CommandDistributor()
+		{
+			_commands = new ConcurrentBag<IMessageBase>();
+		}
+
+		public void Add(IMessageBase command)
+		{
+			_commands.Add(command);
+		}
+
+		public void Distribute()
+		{
+			_bus.Send(_commands);
+		}
+	}
+}
