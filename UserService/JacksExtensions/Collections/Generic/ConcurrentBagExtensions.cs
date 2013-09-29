b30b32297d88dbdb87ab670f627680300ef9faf3
jacan <jckandersen@gmail.com>
2013-09-29 16:26:26 +0200

    Added extensions library

---
+++ UserService/JacksExtensions/Collections/Generic/ConcurrentBagExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JacksExtensions.Collections.Generic
+{
+	public static class ConcurrentBagExtensions
+	{
+		public static void Purge<T>(this ConcurrentBag<T> bag)
+		{
+			T itemRemoved;
+
+			if (bag.Count > 0)
+			{
+				while (bag.TryTake(out itemRemoved)) { }
+			}
+		}
+	}
+}
