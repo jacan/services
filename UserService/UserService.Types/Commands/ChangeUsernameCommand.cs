501c992f96032f9194980a392af6ddc10d7e1ab1
jacan <jckandersen@gmail.com>
2013-10-22 16:22:36 +0200

    Minor changes to Types/Messages

--- UserService/UserService.Types/Commands/ChangeUsernameCommand.cs
+++ UserService/UserService.Types/Commands/ChangeUsernameCommand.cs
@@ -1,17 +1,15 @@
 ﻿using JacksFramework.Service;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace UserService.Types.Commands
 {
-    public class ChangeUsernameCommand : CommandBase
-    {
-        public Guid UserId { get; set; }
-        
-        public string UserName { get; set; }
-        public string NewUserNameRequested { get; set; }
-    }
+	public class ChangeUsernameCommand : ICommandMessage
+	{
+		public Guid Id { get; set; }
+		public DateTime? Timestamp { get; set; }
+		
+		public Guid UserId { get; set; }
+		public string UserName { get; set; }
+		public string NewUserNameRequested { get; set; }
+	}
 }
--- UserService/UserService.Types/NewBasicUser.cs
+++ UserService/UserService.Types/NewBasicUser.cs
@@ -6,15 +6,15 @@ using System.Threading.Tasks;
 
 namespace UserService.Types
 {
-    public interface IBasicUser
-    {
-        string Username { get; set; }
-        string Email { get; set; }
-    }
+	public interface IBasicUser
+	{
+		string Username { get; set; }
+		string Email { get; set; }
+	}
 
-    public class NewBasicUser
-    {
-        public string Username { get; set; }
-        public string Email { get; set; }
-    }
+	public class NewBasicUser
+	{
+		public string Username { get; set; }
+		public string Email { get; set; }
+	}
 }
