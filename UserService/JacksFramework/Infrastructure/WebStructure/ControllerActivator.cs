31c2e09da4e436e65dacf376e90bf556ab99235b
jacan <jckandersen@gmail.com>
2013-10-30 12:01:09 +0100

    Added some web infrastructure with containers

---
+++ UserService/JacksFramework/Infrastructure/WebStructure/ControllerActivator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace JacksFramework.Infrastructure.WebStructure
+{
+	public class ControllerActivator : IControllerActivator
+	{
+		public IController Create(System.Web.Routing.RequestContext requestContext, Type controllerType)
+		{
+			return DependencyResolver.Current.GetService(controllerType) as IController;
+		}
+	}
+}
---
+++ UserService/JacksFramework/Infrastructure/WebStructure/StructureMapDependencyResolver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using StructureMap;
+
+namespace JacksFramework.Infrastructure.WebStructure
+{
+	public class StructureMapDependencyResolver : IDependencyResolver
+	{
+		private readonly IContainer _container;
+
+		public StructureMapDependencyResolver(IContainer container)
+		{
+			_container = container;
+		}
+
+		public object GetService(Type serviceType)
+		{
+			if (serviceType.IsAbstract || serviceType.IsInterface)
+			{
+				return _container.TryGetInstance(serviceType);
+			}
+
+			return _container.GetInstance(serviceType);
+		}
+
+		public IEnumerable<object> GetServices(Type serviceType)
+		{
+			return _container.GetAllInstances<object>().Where(x => x.GetType() == serviceType);
+		}
+	}
+}
