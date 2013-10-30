b478d4183a4b89ad3a0fd9e27c2e0436cd4516f4
jacan <jckandersen@gmail.com>
2013-10-30 12:27:48 +0100

    Added invalid timestamp exception

---
+++ UserService/JacksFramework/Service/Endpoints/MessageTimestampInvalidException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JacksFramework.Service.Endpoints
+{
+	public class MessageTimestampInvalidException : Exception
+	{
+		public MessageTimestampInvalidException()
+			: base("Message timestamp must be set in order to send it on the bus!")
+		{
+			
+		}
+	}
+}
