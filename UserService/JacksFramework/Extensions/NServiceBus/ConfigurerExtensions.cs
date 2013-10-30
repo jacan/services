8e7ba743c79ad2a455f198fdd6d80c6237ab125a
jacan <jckandersen@gmail.com>
2013-10-30 12:28:36 +0100

    Added extension for NServiceBus configurer

---
+++ UserService/JacksFramework/Extensions/NServiceBus/ConfigurerExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+using NServiceBus;
+
+using JacksFramework.Infrastructure.WebStructure;
+
+namespace JacksFramework.Extensions.NServiceBus
+{
+	public static class ConfigurererExtensions
+	{
+		/// <summary>
+		/// Configures MVC (v4)
+		/// </summary>
+		/// <param name="configure"></param>
+		/// <returns></returns>
+		public static Configure ForMvc(this Configure configure)
+		{
+			configure.Configurer.RegisterSingleton(typeof(IControllerActivator), new ControllerActivator());
+
+			var controllers = Configure.TypesToScan.Where(x => typeof(IControllerActivator).IsAssignableFrom(x));
+
+			foreach(Type type in controllers)
+			{
+				configure.Configurer.ConfigureComponent(type, DependencyLifecycle.InstancePerCall);
+			}
+
+			return configure;
+		}
+	}
+}
