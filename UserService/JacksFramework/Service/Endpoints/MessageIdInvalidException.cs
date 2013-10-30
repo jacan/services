1be9558e4d395132eee637bf402733a0e7259452
jacan <jckandersen@gmail.com>
2013-10-30 12:26:36 +0100

    Added message id invalid exception

---
+++ UserService/JacksFramework/Service/Endpoints/MessageIdInvalidException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JacksFramework.Service.Endpoints
+{
+	public class MessageIdInvalidException : Exception
+	{
+		public MessageIdInvalidException()
+			: base("Message Id must be set, in order to send it on the bus!")
+		{
+			
+		}
+	}
+}
