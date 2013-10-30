bf9330af9a239743ea14c1354e9c916a1076cdcc
jacan <jckandersen@gmail.com>
2013-10-30 12:44:39 +0100

    A basic command against a user

---
+++ UserService/UserService.Types/Commands/IUserCommand.cs
@@ -0,0 +1,14 @@
+﻿using JacksFramework.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserService.Types.Commands
+{
+	public interface IUserCommand : ICommandMessage
+	{
+
+	}
+}
