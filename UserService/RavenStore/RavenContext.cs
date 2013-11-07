9293884e31a1191b78acb3401aa4625330f5cbaa
jacan <jckandersen@gmail.com>
2013-11-07 20:26:57 +0100

    Updated raven store to a minimum of throughput

--- UserService/RavenStore/RavenContext.cs
+++ UserService/RavenStore/RavenContext.cs
@@ -6,32 +6,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using UserService.Store;
 
 namespace RavenStore
 {
-    public class RavenContext : IRepositoryContext, IDisposable
-    {
-        IDocumentStore _documentStore;
+	public class RavenContext : IRepositoryContext, IDisposable
+	{
+		IDocumentStore _documentStore;
 
-        public RavenContext(string connectionString="http://www.localhost/:8080")
-        {
-            _documentStore = new DocumentStore();            
-            _documentStore.Initialize();
-            
-        }
+		public RavenContext(string connectionString)
+		{
+			_documentStore = new DocumentStore { Url = connectionString };
+			_documentStore.Initialize();
 
-        public void Dispose()
-        {
-            _documentStore.Dispose();
-        }
+		}
 
-        public void Add(dynamic entity)
-        {
-            using (var session = _documentStore.OpenSession())
-            {
-                session.Store(entity);
-            }
-        }
-    }
+		public void Dispose()
+		{
+			_documentStore.Dispose();
+		}
+
+		public void Add(dynamic entity)
+		{
+			using (var session = _documentStore.OpenSession())
+			{
+				session.Store(entity);
+			}
+		}
+	}
 }
